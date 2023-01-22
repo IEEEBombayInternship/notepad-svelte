fc6b74155088ec8e7d1f050d7ab11c9a292d7508
bigdwarf43 <yashwankhedkar43@gmail.com>
2023-01-22 11:03:55 +0530

    v1

--- src/routes/editor.svelte
+++ src/routes/editor.svelte
@@ -1,28 +1,75 @@
 <!-- This script makes a quill js instance and binds it to a div
 Edit this script to make changes to the editor window -->
 
-<script>
+<script >
+// @ts-nocheck
+
 	import {  onMount } from 'svelte'
 	import Quill from "quill";
+  //v2
+  import HandleUser from './handleUsers.svelte';
+  let userHandler;
     
 	let quill = null;
+  let docTitle ='';
     // @ts-ignore
   export let container = null;
+
+  var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script':'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean'],                                         // remove formatting button
+  ['link', 'image', 'video'],
+  ['spanblock']
+];
+
+
+  //v2
+  function handleSave(){
+
+
+    let text = quill.getText();
+    userHandler.addData(docTitle, text);
+
+   
+  }
 	
   onMount( () => { 
 		// @ts-ignore
 		quill = new Quill(container, {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, 3, false] }],
-        ["bold", "italic", "underline", "strike"],
-        ["link", "code-block"]
-      ]
-    },
-    placeholder: "Type something...",
-    theme: "snow" // or 'bubble'
+      modules: {
+    toolbar: toolbarOptions
+  },
+  theme: 'snow',
+    placeholder: "Type something..."
   });
-	})
+	
+  
+  })
+
+
+
+  export function setEditorText(text){
+    quill.setText(text)
+  }
+
+  export function updateTitle(title){
+    docTitle = title
+  }
 </script>
 
 <style>
@@ -33,19 +80,65 @@ Edit this script to make changes to the editor window -->
     margin: 0 auto;
   }
 
-  #editor {
-    height: 350px;
-  }
-
   @media (min-width: 640px) {
     main {
       max-width: none;
     }
   }
+
+
+  
+  #editor{
+   height: 80vh;  
+   width: 50wh; 
+   }
+
+   .title{
+    border: none; 
+  font-size: xx-large;
+  background-color: #D9D9D9;
+  color: #000000;
+  font-family:Arial, Helvetica, sans-serif;
+  border-radius: 10px;
+  margin-left: 10px;
+   }
+ 
+
+
+   .save{
+  background-color: #0066FF;
+  color: #ffffff;
+  border: none;
+  font-size: 1.3em;
+  font-weight: 300;
+  position: relative;
+  outline: none;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  height: 30px;
+  width: 70px;
+}
+
+.save:hover{
+color: #000000;
+  background-color: #ffffff;
+  text-decoration: none;
+}
+
 </style>
 
 <svelte:head>
 	<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 </svelte:head>
-
+<div style="background-color: #D9D9D9; border-radius:10px">
+  <input type="text" bind:value={docTitle} class="title" placeholder="Untitled Document"/>
+  <button class="save" on:click={handleSave}>save</button>
+</div>
+<br>
 <div id="editor" bind:this={container}/>
+<!-- 
+v2 -->
+
+<HandleUser bind:this={userHandler}></HandleUser>
---
+++ src/routes/handleUsers.svelte
@@ -0,0 +1,40 @@
+
+<script>
+// @ts-nocheck
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore"; 
+import { collection, query, setDoc, where, getDocs ,onSnapshot, doc, updateDoc, deleteDoc, addDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
+const auth = getAuth();
+let user = (auth.currentUser).email
+
+const firebaseConfig = {
+        apiKey: "AIzaSyApAJewyPTQZmA3A83fmK2NrdkyB48PYWA",
+        authDomain: "svelte-todo-3ff8e.firebaseapp.com",
+        projectId: "svelte-todo-3ff8e",
+        storageBucket: "svelte-todo-3ff8e.appspot.com",
+        messagingSenderId: "48913790170",
+        appId: "1:48913790170:web:c3d5d29823f804b6aa21e3",
+        measurementId: "G-XFML1S3ZSE"
+	};
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore();
+console.log(db);
+const colRef =  query(collection(db, "userDocs"));
+
+
+export const addData = async(title, text) => {
+    await setDoc(doc(db, "userDocs", title+""+user), {
+            username:  user,
+            content: String(text),
+            title: title
+            });
+            location.reload()
+}
+
+
+</script>
